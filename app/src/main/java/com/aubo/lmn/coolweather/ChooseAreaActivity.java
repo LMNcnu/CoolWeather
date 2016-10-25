4892218363b789852d00b87c4a6c43001322c26c
lmn <1169940335@qq.com>
2016-10-25 10:16:27 +0800

    CoolWeather app project is created

---
+++ app/src/main/java/com/aubo/lmn/coolweather/ChooseAreaActivity.java
@@ -0,0 +1,13 @@
+package com.aubo.lmn.coolweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ChooseAreaActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_area);
+    }
+}
